856fa68b5fc6b332e271722903eaea5b7baccc78
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2025-06-25 09:17:59 -0400

    Added OS switch for dynlib extensions

--- build.zig
+++ build.zig
@@ -2,6 +2,13 @@ const std = @import("std");
 
 const name = "zzonklib";
 
+const system_lib_ext = switch (@import("builtin").os.tag) {
+    .windows => ".dll",
+    .macos => ".dylib",
+    .wasi, .emscripten => ".wasm",
+    else => ".so",
+};
+
 pub fn build(b: *std.Build) !void {
     const so = b.addSharedLibrary(.{
         .name = name,
@@ -12,7 +19,7 @@ pub fn build(b: *std.Build) !void {
 
     b.installArtifact(so);
 
-    const rename_lib = b.addSystemCommand(&.{ "mv", "zig-out/lib/lib" ++ name ++ ".dylib", "zig-out/lib/" ++ name ++ ".so" });
+    const rename_lib = b.addSystemCommand(&.{ "mv", "zig-out/lib/lib" ++ name ++ system_lib_ext, "zig-out/lib/" ++ name ++ system_lib_ext });
     rename_lib.step.dependOn(b.getInstallStep());
 
     const build_step = b.step("b", "");
